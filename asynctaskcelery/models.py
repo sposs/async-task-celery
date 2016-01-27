86855072a2052628b74fb0f7e3a357dc19ffd126
stephane poss <stephane@xample.ch>
2016-01-27 17:41:50 +0100

    add relationship between task and data

--- asynctaskcelery/models.py
+++ asynctaskcelery/models.py
@@ -3,15 +3,24 @@ from __future__ import unicode_literals
 from celery.canvas import chord
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
 from asynctaskcelery.tasks import generic_run
+import json
 
 TASKS_CHOICES = (("immediate", _("Immediate")),
                  ("scheduled", _("Scheduled")))
 
 
+##  need to use https://github.com/bradjasper/django-jsonfield
 class Data(models.Model):
     value = models.TextField()
+
+    def get_json(self):
+        """
+        Because we need to get JSON
+        :return:
+        """
+        return json.loads(self.value)
+
 
 # Create your models here.
 class Task(models.Model):
@@ -19,6 +28,7 @@ class Task(models.Model):
     author = models.CharField(max_length=20)
     type = models.CharField(max_length=20, choices=TASKS_CHOICES)
     parents = models.ManyToManyField('Task', verbose_name="List of parents, can be one item")
+    input_data = models.ManyToManyField(Data, verbose_name="Data used by the task")
 
     def get_task(self):
         """
@@ -31,4 +41,4 @@ class Task(models.Model):
                 l_tasks.append(parent.get_task())
             return chord(l_tasks, generic_run.s(task_name=self.task_name))
         else:
-            return generic_run.si(self.input_data, task_name=self.task_name)
+            return generic_run.si(self.input_data.get_json(), task_name=self.task_name)
